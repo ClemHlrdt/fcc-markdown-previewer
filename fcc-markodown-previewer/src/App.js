119bf0b2ec5e756293745991b6fc3c2d4b4fad12
Clément Hélardot <clement.helardot@gmail.com>
2019-09-19 21:26:00 +0200

    added navbar and styles

--- fcc-markodown-previewer/src/App.js
+++ fcc-markodown-previewer/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MarkdownPreviewer from './components/MarkdownPreviewer/MarkdownPreviewer'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MarkdownPreviewer />
     </div>
   );
--- fcc-markodown-previewer/src/components/MarkdownPreviewer/MarkdownPreviewer.js
+++ fcc-markodown-previewer/src/components/MarkdownPreviewer/MarkdownPreviewer.js
@@ -3,6 +3,12 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './MarkdownPreviewer.css';
 const marked = require('marked');
 
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = (new JSDOM('')).window;
+const DOMPurify = createDOMPurify(window);
+
 class MarkdownPreviewer extends Component {
     state = { 
         markdown:"# Welcome to Markdown Previewer\n## Markdown is fun\n\n[Learn basic syntax now](https://www.markdownguide.org/basic-syntax/)\nInline `code` has `back-ticks around` it\nYou can input some code\n```javascript\nconsole.log('Hello World')\n```\nYou display lists of items\n1. Buy bread\n1. Cut bread\n1. Add ingredients\n### Add blockquotes\n> a blockquote\n### Images\n![The markdown logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/200px-Markdown-mark.svg.png)\n### Bold text\nThis is **bold**"
@@ -17,22 +23,20 @@ class MarkdownPreviewer extends Component {
     render() {
         const markdown = this.state.markdown;
         return (
-            <div className="container">
-                <div className="row">
+
+                    <div className="row content">
                     <div className="col-md-6 panel editor">
-                        <Form>
-                            <FormGroup>
-                                <Label for="exampleText">Text Area</Label>
-                                    <Input type="textarea" name="editor" id="editor" cols="30" rows="28" value={this.state.markdown} onChange={this.updateMarkdown}/>
-                                </FormGroup>
-                            {/* <textarea name="editor" id="editor" cols="30" rows="10" ></textarea> */}
-                        </Form>
+                        <div className="editor__title editor__title-dark">Your markdown</div>
+                                    <textarea className="editor__textarea editor__textarea-dark" name="editor" id="editor" cols="30" rows="10" value={this.state.markdown} onChange={this.updateMarkdown}></textarea>
+                                
                     </div>
                     <div className="col-md-6 panel previewer">
-                        <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdown, {breaks:true,sanitize: true})}} />
-                    </div>
+                        <div className="preview__title">Your result</div>
+                            <div className="preview__text preview__text-light" id="preview" dangerouslySetInnerHTML={{__html: marked(DOMPurify.sanitize(markdown), {breaks:true})}} />
+                        </div>
                 </div>
-            </div>
+
+
         );
     }
 }
